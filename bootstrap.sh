82ccea769a2b6026f7d0c282fe1d132f26dbf42f
d1ffuz0r <d1fffuz0r@gmail.com>
2012-08-12 19:33:03 +0400

    new installation script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
 PROJECT_NAME=$1
-PATH_TO_TEMPLATE=https://github.com/d1ffuz0r/djrails/zipball/master
+REPO=git://github.com/d1ffuz0r/djrails.git
+
+if [ ! -d djrails ]; then
+    git clone git://github.com/d1ffuz0r/djrails.git
+fi;
 
 if [ ! -d $PROJECT_NAME ]; then
-    django-admin.py startproject --template $PATH_TO_TEMPLATE --extension py,md,txt $PROJECT_NAME -v 2
+    django-admin.py startproject --template ./djrails/project_name $PROJECT_NAME -v 2
     echo "project $PROJECT_NAME was created"
     pip install -r $PROJECT_NAME/pipfile.txt
 else
     echo "project $PROJECT_NAME already exists"
+fi;
+
+if [ -d djrails ]; then
+    rm -rf djrails
 fi;

1453a5aa2172ba6e7d227f969609026f2d232e28
d1ffuz0r <d1fffuz0r@gmail.com>
2012-08-12 19:14:34 +0400

    fixed installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,9 @@ PROJECT_NAME=$1
 PATH_TO_TEMPLATE=https://github.com/d1ffuz0r/djrails/zipball/master
 
 if [ ! -d $PROJECT_NAME ]; then
-    django-admin.py startproject $PROJECT_NAME --template $PATH_TO_TEMPLATE -v 2
+    django-admin.py startproject --template $PATH_TO_TEMPLATE --extension py,md,txt $PROJECT_NAME -v 2
     echo "project $PROJECT_NAME was created"
-    pip install -r $PROJECT_NAME/pipfile
+    pip install -r $PROJECT_NAME/pipfile.txt
 else
     echo "project $PROJECT_NAME already exists"
 fi;
